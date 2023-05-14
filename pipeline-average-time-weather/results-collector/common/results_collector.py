0ed884ed2f2fc10d76c7894c80b12230fe1189c7
mateocapon <mcapon@fi.uba.ar>
2023-05-14 11:42:15 -0300

    res collector

--- pipeline-average-time-weather/results-collector/common/results_collector.py
+++ pipeline-average-time-weather/results-collector/common/results_collector.py
@@ -1,39 +1,27 @@
-import pika
+from common.results_collector_middleware import ResultsCollectorMiddleware
 import logging
+import signal
 
 RESULTS_AVERAGE_DURATION = b'A'
 
 class ResultsCollector:
     def __init__(self, n_average_processes):
-        self._connection = pika.BlockingConnection(
-                                pika.ConnectionParameters(host='rabbitmq', heartbeat=1200))
-        self._channel = self._connection.channel()
+        self._middleware = ResultsCollectorMiddleware()
         self._n_average_processes = n_average_processes
         self._results_received = 0
-
-        # results to consume and collect
-        self._channel.queue_declare(queue='results-collector-average-duration', durable=True)
-
-        # results to produce result to client
-        self._channel.queue_declare(queue='final-results', durable=True)
-        self._results = b''
+        self._results = RESULTS_AVERAGE_DURATION
+        signal.signal(signal.SIGTERM, self.__stop_working)
 
     def run(self):
-        self._channel.basic_consume(queue='results-collector-average-duration', on_message_callback=self.__callback, auto_ack=True)
-        self._channel.start_consuming()
-        logging.info(f"Termine de consumir durations")
+        self._middleware.receive_results(self.__callback)
 
-    def __callback(self, ch, method, properties, body):
+    def __callback(self, body):
         logging.info(f"action: average_duration_results | result: received")
         self._results_received += 1
         self._results += body
         if self._results_received == self._n_average_processes:
-            self._channel.basic_publish(
-                exchange='',
-                routing_key='final-results',
-                body=RESULTS_AVERAGE_DURATION + self._results
-            )
-            self._channel.stop_consuming()
+            self._middleware.send_results(self._results)
+            self._middleware.stop_receiving()
      
-    def __del__(self):
-        self._connection.close()
+    def __stop_working(self, *args):
+        self._middleware.stop()
---
+++ pipeline-average-time-weather/results-collector/common/results_collector_middleware.py
@@ -0,0 +1,24 @@
+from common.middleware import Middleware
+import logging
+
+class ResultsCollectorMiddleware(Middleware):
+    def __init__(self):
+        super().__init__()
+        # results to consume and collect
+        self._channel.queue_declare(queue='results-collector-average-duration', durable=True)
+
+        # results to produce result to client
+        self._channel.queue_declare(queue='final-results', durable=True)
+
+
+    def receive_results(self, callback):
+        queue='results-collector-average-duration'
+        self.receive_data(callback, queue)
+       
+
+    def send_results(self, results):
+        self._channel.basic_publish(
+                exchange='',
+                routing_key='final-results',
+                body=results
+            )
